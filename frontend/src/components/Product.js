92fbed7cb9bec9569e0a231ff88a60db36e6c7df
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-15 11:04:14 +0300

    render component with data obtained dynamically from the store

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import { Card, Button, Col } from "react-bootstrap";
+import { connect } from "react-redux";
+import * as actionTypes from "../store/actions/actionTypes";
+import { Link } from "react-router-dom";
 
 class Product extends Component {
+
+
   render() {
     return (
       <Col md={4} className="mt-4">
         <Card style={{ width: "18rem" }}>
-          <Card.Img
-            variant="top"
-            src={this.props.item.image}
-            alt={this.props.item.name}
-          />
+          <Link to={"products/" + this.props.item._id}>
+            <Card.Img
+              variant="top"
+              src={`http://localhost:5000/${this.props.item.productImage}`}
+              alt={this.props.item.name}
+            />
+          </Link>
           <Card.Body>
             <Card.Title>{this.props.item.name}</Card.Title>
             <Card.Text> </Card.Text>
-            <Button className="btn-sm float-left" variant="primary">
+            <Button
+              className="btn-sm float-left"
+              variant="primary"
+              onClick={() => this.props.addToCart(this.props.item)}
+            >
               Add To Cart
             </Button>
             <h3 className=" float-right" variant="primary">
               $ {this.props.item.price}
             </h3>
+            <button onClick={() => console.log(this.props.cart)}>cart</button>
           </Card.Body>
         </Card>
       </Col>
@@ -27,4 +39,16 @@ class Product extends Component {
   }
 }
 
-export default Product;
+const mapStateToProps = (state) => {
+  return {
+    cart: state.productReducer.cart,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (item) => dispatch(actionTypes.addToCart(item)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
