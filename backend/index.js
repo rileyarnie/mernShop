679030eb41f16be99a43126b79650dc14bd5f15a
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-15 12:02:34 +0300

    register authentication routes

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const productRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
 
 app.use(express.json());
 app.use(cors());
-app.use("/uploads",express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 app.use("/products", productRoutes);
+app.use("/auth", authRoutes);
 
 app.use(async (req, res, next) => {
   next(createError.NotFound());
