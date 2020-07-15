e6eb8646f1b7b53d392a630db773d1423848ac97
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-15 11:11:53 +0300

    render respective components for specific routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,17 @@ import "./App.css";
 import Navigaton from "./components/Navigation";
 import ProductList from "./components/ProductList";
 import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails";
 function App() {
   return (
     <div className="App">
       <Navigaton />
       <Container>
-        <ProductList />
+        <Switch>
+          <Route exact component={ProductList} path="/" />
+          <Route exact path="/products/:id" component={ProductDetails} />
+        </Switch>
       </Container>
     </div>
   );
