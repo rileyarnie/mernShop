5d2ebe2dadbcbf6cab43ff7a5c5fbaaeb1182f5b
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-15 11:06:18 +0300

    user browser links on the navbar to avoid re-rendering of the DOM

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 export class Navigation extends Component {
   render() {
     return (
       <>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">iSuperStore</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+              iSuperStore
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">
-                <img src="https://img.icons8.com/pastel-glyph/24/000000/shopping-cart.png" />
-              </Nav.Link>
+              <Link
+                to="/"
+                className="mr-4"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                Home
+              </Link>
+              <Link
+                to="/cart"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <img
+                  src="https://img.icons8.com/pastel-glyph/24/000000/shopping-cart.png"
+                  alt="cart"
+                />
+
+                <strong className="ml-1">
+                  {this.props.cart.length < 1 ? "" : this.props.cart.length}
+                </strong>
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
@@ -21,5 +43,10 @@ export class Navigation extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    cart: state.productReducer.cart.items,
+  };
+};
 
-export default Navigation;
+export default connect(mapStateToProps)(Navigation);
