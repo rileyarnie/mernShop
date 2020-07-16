f3ba11a605d61f42585d913f6dbde740719bdd28
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-16 18:31:38 +0300

    define register, login and logout action types and their behaviour

--- frontend/src/store/actions/actionTypes.js
+++ frontend/src/store/actions/actionTypes.js
@@ -25,3 +25,56 @@ export const addToCart = (item) => {
     payload: item,
   };
 };
+
+export const authSignup = (username, email, password) => (dispatch) => {
+  dispatch(authStart());
+  axios
+    .post(`${BASE_URL}auth/register`, { username, email, password })
+    .then((res) => {
+      dispatch(authSuccess(res.data.access_token));
+    })
+    .catch((err) => {
+      dispatch(authFail(err.response.data));
+    });
+};
+
+export const authLogin = (email, password) => (dispatch) => {
+  dispatch(authStart());
+  axios
+    .post(`${BASE_URL}auth/login`, { email, password })
+    .then((res) => {
+      dispatch(authSuccess(res.data.access_token));
+    })
+    .catch((err) => {
+      dispatch(authFail(err.response.data));
+    });
+};
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START,
+  };
+};
+
+export const authSuccess = (token) => {
+  localStorage.setItem("access_token", token);
+
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    payload: token,
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    payload: error,
+  };
+};
+
+export const authLogout = () => {
+  localStorage.removeItem("accessToken");
+  return {
+    type: actionTypes.AUTH_LOGOUT,
+  };
+};
