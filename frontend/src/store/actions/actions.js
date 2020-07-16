e9454d6fb3642e50cc6bb73c6733e90b4432d646
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-16 18:33:25 +0300

    definte authenticationn action constants

--- frontend/src/store/actions/actions.js
+++ frontend/src/store/actions/actions.js
@@ -1,2 +1,8 @@
-export const GET_PRODUCTS = 'GET_PRODUCTS'
-export const ADD_TO_CART = 'ADD_TO_CART'
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const ADD_TO_CART = "ADD_TO_CART";
+
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_LOGOUT = "AUTH_LOGOUT";
+
