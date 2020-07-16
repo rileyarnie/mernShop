8e0119c058b0c5b1c1299c1b501a9fcdb9ceb1de
Arnold Kibira <arnold.ril3y@gmail.com>
2020-07-16 18:32:12 +0300

    register authreducer in the rootreducer

--- frontend/src/store/reducer/index.js
+++ frontend/src/store/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import productReducer from "./products";
+import authReducer from "./auth"
 
 const rootReducer = combineReducers({
  productReducer,
+ authReducer
 });
 
 export default rootReducer;
